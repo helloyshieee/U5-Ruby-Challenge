b4ee95021b552bc4de4de8845ba08aa56a722e0c
Ashley Villalobos <aashley.villalobos@outlook.com>
2023-02-20 13:23:15 -0600

    Populated the products table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+# Create 676 fake products
+676.times do
+  title = Faker::Commerce.product_name
+  price = Faker::Commerce.price(range: 0..100.0)
+  stock_quantity = Faker::Number.between(from: 0, to: 1000)
+  Product.create(title: title, price: price, stock_quantity: stock_quantity)
+end
